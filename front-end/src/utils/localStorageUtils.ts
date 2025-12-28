528f036d7ca1b6604abf2c31fc5c74b741478bad
henluo <122192113+hihenluo@users.noreply.github.com>
2025-12-28 15:13:07 +0000

    feat: update framework file and change network to mainnet

--- front-end/src/utils/localStorageUtils.ts
+++
@@ -1,6 +0,0 @@
-export function storeTxid(txid: string) {
-  const existingTxids = localStorage.getItem('txid')
-    ? JSON.parse(localStorage.getItem('txid')!)
-    : {};
-  localStorage.setItem('txid', JSON.stringify({ ...existingTxids, txid }));
-}
